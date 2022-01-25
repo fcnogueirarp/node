c42e69793348a28f10a09a0c655c83958f9b6a75
Fernnado <fcnogueirarp@gmail.com>
2022-01-25 11:56:51 -0300

    project is finished

--- 11_projeto_pensamentos/controllers/ToughtController.js
+++ 11_projeto_pensamentos/controllers/ToughtController.js
@@ -1,14 +1,40 @@
 const Tought = require('../models/Tought');
 const User = require('../models/User');
+const { Op } = require('sequelize');
 
 module.exports = class ToughtController {
   static async showToughts(req, res) {
+    let search = '';
+
+    if (req.query.search) {
+      search = req.query.search;
+    }
+
+    let order = 'DESC';
+
+    if (req.query.order === 'old') {
+      order = 'ASC';
+    } else {
+      order = 'DESC';
+    }
+
     const toughtsData = await Tought.findAll({
       include: User,
+      where: {
+        title: { [Op.like]: `%${search}%` },
+      },
+      order: [['createdAt', order]],
     });
+
     const toughts = toughtsData.map(result => result.get({ plain: true }));
 
-    await res.render('toughts/home', { toughts });
+    let toughtsQty = toughts.length;
+
+    if (toughtsQty === 0) {
+      toughtsQty = false;
+    }
+
+    await res.render('toughts/home', { toughts, search, toughtsQty });
   }
 
   static async updateTought(req, res) {
